21bfa955f784d1226e9c762ce6f32279bdf7895e
Amirhossein Biglari <amir.h.biglari.79@gmail.com>
2026-05-14 17:00:34 +0330

    Authentication part 2

---
+++ auth.py
@@ -0,0 +1,67 @@
+from datetime import UTC, datetime, timedelta
+
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+
+from config import settings
+
+# creates a password hasher using argon2 with the recommended settings
+password_hash = PasswordHash.recommended()
+
+# This tokenurl has to mathc our login endpoint path
+# OAuth2PasswordBearer extracts the token from the authorization header, when a client sends that, the schema extracts 
+# that token for it.
+# Side effect? this enables the authorize button in our docs, which makes testing authentication a lot easier.
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/users/token")
+
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+# Question: why hashing instead of encryption?
+# Answer: encryption is reversable but hashing is not, even if database is stolen, passwords can't be recovered from hashes.
+# argon2 generates a random seed for each hash, so the same password produces different hashes each time.
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    """Create a jwt access token"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(UTC) + expires_delta
+    else:
+        expire = datetime.now(UTC) + timedelta(
+            minutes=settings.access_token_expire_minutes
+        )
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.secret_key.get_secret_value(),
+        algorithm=settings.algorithm,
+    )
+    return encoded_jwt
+
+
+def verify_access_token(token: str) -> str | None:
+    """Verifying a JWT access token and return the subject (user id) if valid"""
+    try:
+        payload = jwt.decode(
+            token,
+            settings.secret_key.get_secret_value(),
+            algorithms=[settings.algorithm],
+            options={'require': ['exp', 'sub']},
+        )
+    except jwt.InvalidTokenError:
+        return None
+    else:
+        return payload.get('sub')
+    
+
+# JWT structure:
+# header: contains the algorithm and type
+# payload: contains our data + expiration
+# signature: proves the toke wasn't (tampored with)?? 
